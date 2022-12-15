cb114cb5ff6a63f5808493667fb132f30c62e857
goodcodemakers <113665732+goodcodemakers@users.noreply.github.com>
2022-12-15 17:52:00 +0900

    time bgimg add
    time bgimg add

--- src/app.js
+++ src/app.js
@@ -19,12 +19,12 @@ function getWeatherData(cityname = "seoul") {
 console.log(API_URL);
 function showWeather(data) {
   let desc = data.weather[0].main; // 날씨상태 설명
-  const weather_icon = data.weather[0].icon; // 아이콘
+  // const weather_icon = data.weather[0].icon; // 아이콘
   const temp = parseInt(data.main.temp - 273.15); // 현재온도,섭씨
   const name = data.name; // 도시명
-  console.log(name, desc, weather_icon, temp);
-  const bgimgDesc = document.querySelector("body");
-  bgimgDesc.style.backgroundImage = `url(./imges/${desc}.jpg)`;
+  // console.log(name, desc, weather_icon, temp);
+  const bgimgDesc = document.querySelector("#app");
+
   console.log(desc);
   if (
     desc == "Clear" ||
@@ -38,15 +38,17 @@ function showWeather(data) {
   } else {
     desc = "Dust";
   }
+  bgimgDesc.style.backgroundImage = `url(./imges/${desc}.jpg)`;
 
   // UI 출력(DOM)
   const citynameEl = document.querySelector(".cityname");
-  const iconEl = document.querySelector(".icon");
+
   const tempEl = document.querySelector(".temp");
   const descEl = document.querySelector(".desc");
 
   citynameEl.textContent = name;
-  iconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather_icon}@2x.png' alt='아이콘'/>`;
+
+  // iconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather_icon}@2x.png' alt='아이콘'/>`;
   tempEl.innerHTML = `${temp}&deg;`;
   descEl.textContent = desc;
 }
@@ -61,6 +63,37 @@ select.addEventListener("change", function (e) {
   console.log(e.target.value);
   const cityname = e.target.value;
   getWeatherData(cityname);
+  let container = document.querySelector(".container");
+  container.style.backgroundImage = `url(./imges/${cityname}.jpg)`;
 });
 
 //날씨나 시간대 (주간/야간)에 따라 배경 연출 바꾸기
+function times() {
+  const time = new Date();
+  let hours = time.getHours(); // 시
+  let minutes = time.getMinutes() * 6; //분
+  let seconds = (time.getSeconds() + 1) * 6; //초
+  let sLine = document.querySelector(".sLine");
+  let mLine = document.querySelector(".mLine");
+  let hLine = document.querySelector(".hLine");
+  let city = document.querySelector(".cityname").innerHTML;
+  if (city == "Boston") {
+    hours = hours - 14;
+  } else if (city == "Barcelona") {
+    hours = hours - 8;
+  } else if (city == "Berlin") {
+    hours = hours - 8;
+  } else if (city == "Moscow") {
+    hours = hours - 6;
+  } else if (city == "Algiers") {
+    hours = hours - 8;
+  } else if (city == "Sydney") {
+    hours = hours - 2;
+  } else if (city == "Reykjavik") {
+    hours = hours - 9;
+  }
+  sLine.style.transform = `rotate(${seconds}deg)`;
+  mLine.style.transform = `rotate(${minutes}deg)`;
+  hLine.style.transform = `rotate(${hours * 30}deg)`;
+}
+setInterval(times, 1000);
